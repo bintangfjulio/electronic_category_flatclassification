1287841d3e957b2a377e7fb73bdda9b3ef315347
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2022-12-07 19:35:53 +0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -9,13 +9,13 @@ from models.indobert_cnn import IndoBERT_CNN
 if __name__ == "__main__":
     pl.seed_everything(1234, workers=True)
     dataset = pd.read_csv('datasets/product_tokopedia.csv')
-    num_classes = len(dataset['category'].drop_duplicates().values.tolist())
+    num_classes = len(dataset['leaf'].drop_duplicates().values.tolist())
     
     module = Preprocessor(batch_size=32, dataset=dataset) 
     model = IndoBERT_CNN(lr=2e-5, num_classes=num_classes)
 
-    checkpoint_callback = ModelCheckpoint(dirpath='./checkpoints/indobertcnn_results', monitor='val_loss')
-    logger = TensorBoardLogger("logs", name="indobertcnn_results")
+    checkpoint_callback = ModelCheckpoint(dirpath='./checkpoints/flat_indobertcnn_results', monitor='val_loss')
+    logger = TensorBoardLogger("logs", name="flat_indobertcnn_results")
     early_stop_callback = EarlyStopping(monitor='val_loss', 
                                         min_delta=0.00, 
                                         check_on_train_epoch_end=1, 
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     trainer = pl.Trainer(
         accelerator='gpu',
         max_epochs=100,
-        default_root_dir="./checkpoints/indobertcnn_results",
+        default_root_dir="./checkpoints/flat_indobertcnn_results",
         callbacks = [checkpoint_callback, early_stop_callback],
         logger=logger,
         log_every_n_steps=5)
