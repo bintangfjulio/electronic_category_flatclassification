46ea8f9ae99efe749ad35c5998c758460988f626
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2022-12-08 16:44:36 +0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -28,7 +28,8 @@ if __name__ == "__main__":
         default_root_dir="./checkpoints/flat_indobertcnn_results",
         callbacks = [checkpoint_callback, early_stop_callback],
         logger=logger,
-        log_every_n_steps=5)
+        log_every_n_steps=5,
+        deterministic=True)
 
     trainer.fit(model, datamodule=module)
     trainer.test(model=model, datamodule=module, ckpt_path='best')
