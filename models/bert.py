dedbaeef3910006aa870a6b99c95b3ee43252f24
bintangfjulio <bintangin14@gmail.com>
2022-12-10 17:54:07 +0700

    add other model

--- models/bert.py
+++ models/bert.py
@@ -6,33 +6,27 @@ import torch.nn.functional as F
 from sklearn.metrics import classification_report
 from transformers import BertModel
 
-class BERT_CNN(pl.LightningModule):
-    def __init__(self, lr, num_classes, dropout=0.1, embedding_size=768, window_sizes=[1, 2, 3, 4, 5], filters_in=4, filters_out=32):
-        super(BERT_CNN, self).__init__()
-        self.bert = BertModel.from_pretrained('indolem/indobert-base-uncased', output_hidden_states=True)
-        self.conv_layers = nn.ModuleList([nn.Conv2d(filters_in, filters_out, (window_size, embedding_size)) for window_size in window_sizes])
-        self.lr = lr
+class BERT(pl.LightningModule):
+    def __init__(self, lr, num_classes, dropout=0.1, embedding_size=768, hidden_size=768):
+        super(BERT, self).__init__()
+        self.bert = BertModel.from_pretrained('indolem/indobert-base-uncased')
+        self.pre_classifier = nn.Linear(embedding_size, hidden_size)
+        self.classifier = nn.Linear(hidden_size, num_classes)
         self.dropout = nn.Dropout(dropout)
-        self.classifier = nn.Linear(len(window_sizes) * filters_out, num_classes)
-        self.sigmoid = nn.Sigmoid()
-        self.criterion = nn.BCELoss()
+        self.lr = lr      
+        self.criterion = nn.BCEWithLogitsLoss()
+        self.tanh = nn.Tanh()
 
     def forward(self, input_ids):
         bert_output = self.bert(input_ids=input_ids)
-        bert_hidden_states = bert_output[2]
-        bert_hidden_states = torch.stack(bert_hidden_states, dim=1)
-        bert_hidden_states = bert_hidden_state[:, -4:]
+        bert_last_hiddenstate = bert_output[0]
+        pooler = bert_last_hiddenstate[:, 0]
+        pooler = self.pre_classifier(pooler)
+        pooler = self.tanh(pooler)
+        pooler = self.dropout(pooler)
+        logits = self.classifier(pooler)
 
-        pooler = [F.relu(conv_layer(bert_hidden_states).squeeze(3)) for conv_layer in self.conv_layers] 
-        max_pooler = [F.max_pool1d(output, output.size(2)).squeeze(2) for output in pooler]  
-
-        flatten = torch.cat(max_pooler, dim=1) 
-
-        fully_connected = self.dropout(flatten)
-        fully_connected = self.classifier(fully_connected)
-        output = self.sigmoid(fully_connected)
-
-        return output
+        return logits
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
@@ -68,7 +62,7 @@ class BERT_CNN(pl.LightningModule):
         return loss
 
     def test_step(self, test_batch, batch_idx):
-        x_input_ids, target = test_batch
+        input_ids, target = test_batch
 
         output = self(input_ids=input_ids)
         loss = self.criterion(output.cpu(), target=target.float().cpu())
--- utils/preprocessor.py
+++ utils/preprocessor.py
@@ -11,11 +11,11 @@ from transformers import BertTokenizer
 from torch.utils.data import TensorDataset, DataLoader
 
 class Preprocessor(pl.LightningDataModule):
-    def __init__(self, batch_size, dataset, hierarchy='datasets/label_hierarchy.tree'):
+    def __init__(self, batch_size, dataset, num_classes):
         super(Preprocessor, self).__init__()
         self.batch_size = batch_size
         self.dataset = dataset
-        self.hierarchy = hierarchy
+        self.num_classes = num_classes
         self.stop_words = StopWordRemoverFactory().get_stop_words()
         self.stemmer = StemmerFactory().create_stemmer()
         self.tokenizer = BertTokenizer.from_pretrained('indolem/indobert-base-uncased')
@@ -76,81 +76,40 @@ class Preprocessor(pl.LightningDataModule):
         print("Max Length:", max_length)
         
         return max_length
-        
-    def generate_hierarchy(self):
-        hierarchy_of_level = {}
-        level_of_hierarchy = {}
-        
-        with open(self.hierarchy, "r") as raw_hierarchy:
-            for row in raw_hierarchy:
-                row = row[:-1].lower()
-                category = row.split(" > ")[-1]
-                level = len(row.split(" > "))
-                
-                if category not in hierarchy_of_level:
-                    hierarchy_of_level[category] = level
-                    
-                    if level not in level_of_hierarchy:
-                        level_of_hierarchy[level] = []
-                        
-                    level_of_hierarchy[level] += [category]
-                    
-        return hierarchy_of_level, level_of_hierarchy
 
     def preprocessing_data(self, dataset): 
+        input_ids, target = [], []
+
+        label_idx = dataset['leaf'].unique().tolist()
+        dataset['leaf'] = dataset['leaf'].map(lambda x: label_idx.index(x))
+
         max_length = self.get_maxlength(dataset)
-        hierarchy_of_level, level_of_hierarchy = self.generate_hierarchy()
-        
-        x_input_ids, x_attention_mask, flat_target, hierarchy_target = [], [], [], []
 
         for data in dataset.values.tolist():
-            product_name = self.data_cleaning(str(data[0])) 
-            token = self.tokenizer(text=product_name,  
-                                max_length=max_length, 
-                                padding="max_length", 
-                                truncation=True)  
-            
-            flat_label = data[3].split(" > ")[-1]
-            flat_binary_label = [0] * len(level_of_hierarchy[3])
-            flat_binary_label[level_of_hierarchy[3].index(flat_label.lower())] = 1     
-            
-#             hierarchy_binary_label = []
-            
-#             leaf_path = data[3]
-#             for level, category in enumerate(leaf_path.split(" > "), 1):
-#                 binary = [0] * len(level_of_hierarchy[level])
-#                 binary[level_of_hierarchy[level].index(category.lower())] = 1
-#                 hierarchy_binary_label.append(binary)
-            
-            x_input_ids.append(token['input_ids'])
-            x_attention_mask.append(token['attention_mask'])
-            flat_target.append(flat_binary_label)
-#             hierarchy_target.append(hierarchy_binary_label)
+            name = self.data_cleaning(str(data[0])) 
 
-        x_input_ids = torch.tensor(x_input_ids)
-        x_attention_mask = torch.tensor(x_attention_mask)
-        flat_target = torch.tensor(flat_target)
-        # hierarchy_target = torch.tensor(hierarchy_target)
+            binary_label = [0] * self.num_classes
+            binary_label[int(data[3])] = 1
+
+            token = self.tokenizer(text=name, max_length=max_length, padding="max_length", truncation=True)  
+            
+            input_ids.append(token['input_ids'])
+            target.append(binary_label)
+
+        input_ids = torch.tensor(input_ids)
+        target = torch.tensor(target)
         
-        tensor_dataset = TensorDataset(x_input_ids, x_attention_mask, flat_target)
+        tensor_dataset = TensorDataset(input_ids, target)
 
         train_valid_size = round(len(tensor_dataset) * 0.8)
         test_size = len(tensor_dataset) - train_valid_size
 
-        train_valid_set, test_set = torch.utils.data.random_split(
-            tensor_dataset, [
-                train_valid_size, test_size
-            ]
-        )
+        train_valid_set, test_set = torch.utils.data.random_split(tensor_dataset, [train_valid_size, test_size])
 
         train_size = round(len(train_valid_set) * 0.9)
         valid_size = len(train_valid_set) - train_size
 
-        train_set, valid_set = torch.utils.data.random_split(
-            train_valid_set, [
-                train_size, valid_size
-            ]
-        )
+        train_set, valid_set = torch.utils.data.random_split(train_valid_set, [train_size, valid_size])
 
         torch.save(train_set, "datasets/train_set.pt")
         torch.save(valid_set, "datasets/valid_set.pt")
@@ -168,5 +127,6 @@ class Preprocessor(pl.LightningDataModule):
         text = re.sub(r'\d+', '', text)
         text = ' '.join([word for word in text.split() if word not in self.stop_words])
         text = text.strip()
+        text = self.stemmer.stem(text)
 
-        return self.stemmer.stem(text)
+        return text
