f3406f68b24a45b1c724393390819e9da3b64413
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2022-12-16 20:49:38 +0700

    Update bert_cnn.py

--- models/bert_cnn.py
+++ models/bert_cnn.py
@@ -25,6 +25,6 @@ class BERT_CNN(pl.LightningModule):
 
         flatten = torch.cat(max_pooler, dim=1) 
         fully_connected_layer = self.fully_connected(self.dropout(flatten))
-        logits = self.sigmoid(fully_connected_layer)
+        output = self.sigmoid(fully_connected_layer)
 
-        return logits
+        return output
