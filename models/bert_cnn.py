473fbd25435e916d52dfcb50c0699297ccf390bc
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2022-12-16 20:45:12 +0700

    Update bert_cnn.py

--- models/bert_cnn.py
+++ models/bert_cnn.py
@@ -9,7 +9,7 @@ class BERT_CNN(pl.LightningModule):
     def __init__(self, num_classes, dropout=0.1, embedding_size=768, window_sizes=[1, 2, 3, 4, 5], filters_in=4, filters_out=32):
         super(BERT_CNN, self).__init__()
         self.bert = BertModel.from_pretrained('indolem/indobert-base-uncased', output_hidden_states=True)
-        self.conv_layers = nn.ModuleList([nn.Conv2d(filters_in, filters_out, (window_size, embedding_size), padding=(window_size-1, 0)) for window_size in window_sizes])
+        self.conv_layers = nn.ModuleList([nn.Conv2d(filters_in, filters_out, (window_size, embedding_size)) for window_size in window_sizes])
         self.dropout = nn.Dropout(dropout)
         self.fully_connected = nn.Linear(len(window_sizes) * filters_out, num_classes)
         self.sigmoid = nn.Sigmoid()
