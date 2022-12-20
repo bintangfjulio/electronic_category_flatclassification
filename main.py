5b3373425177a6210cd8a632c4b718649898ca0c
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2022-12-20 18:17:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', choices=['bert', 'bert-cnn', 'bert-bilstm', 'bert-lstm'], required=True, help='Model choices to fine tune')
     parser.add_argument('--method', choices=['flat', 'hierarchy'], required=True, help='Fine tuning method choices')
+      
+    args = parser.parse_args()
+    config = vars(args)
+    
+    model_path = config['model']
+    method = config['method']
     
     if os.path.exists('datasets/product_tokopedia.zip'):
         with zipfile.ZipFile('datasets/product_tokopedia.zip', 'r') as package:
             package.extractall('datasets')
             
         os.remove('datasets/product_tokopedia.zip')
-    
-    args = parser.parse_args()
-    config = vars(args)
-    
-    model_path = config['model']
-    method = config['method']
 
     dataset = pd.read_csv('datasets/product_tokopedia.csv')
     num_classes = len(dataset['leaf'].drop_duplicates().values.tolist())
