78f3e5907d2d3796ca44acb7c6ae35af4446a84f
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-01-05 22:31:59 +0700

    shorting feed forward

--- models/bert.py
+++ models/bert.py
@@ -6,18 +6,17 @@ from transformers import BertModel
 class BERT(pl.LightningModule):
     def __init__(self, num_classes, dropout=0.1, input_size=768, hidden_size=768):
         super(BERT, self).__init__()
-        self.bert = BertModel.from_pretrained('indolem/indobert-base-uncased')
+        self.pretrained_bert = BertModel.from_pretrained('indolem/indobert-base-uncased')
         self.hidden_layer = nn.Linear(input_size, hidden_size)
-        self.fully_connected = nn.Linear(hidden_size, num_classes)
+        self.output_layer = nn.Linear(hidden_size, num_classes)
         self.dropout = nn.Dropout(dropout)   
         self.tanh = nn.Tanh()
 
     def forward(self, input_ids):
-        bert_output = self.bert(input_ids=input_ids)
-        last_hidden_state = bert_output[0]
-        cls_hidden_state = last_hidden_state[:, 0]
-        pooling_layer = self.hidden_layer(cls_hidden_state)
-        pooled_output = self.tanh(pooling_layer)
-        output = self.fully_connected(self.dropout(pooled_output))
+        bert_output = self.pretrained_bert(input_ids=input_ids)
+        bert_output_layer = bert_output[0]
+        cls_hidden_state = bert_output_layer[:, 0]
+        pooled_output =  self.tanh(self.hidden_layer(cls_hidden_state))
+        output = self.output_layer(self.dropout(pooled_output))
 
         return output
