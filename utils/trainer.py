b4af9de3fa3bb8989f84b798b48085b42a4e5b45
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-01-07 20:13:32 +0700

    change torch metric

--- utils/trainer.py
+++ utils/trainer.py
@@ -4,8 +4,8 @@ import torch.nn as nn
 
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import matthews_corrcoef
+from torchmetrics.classification import BinaryMatthewsCorrCoef
+from torchmetrics.classification import BinaryAccuracy
 from models.bert import BERT
 from models.bert_cnn import BERT_CNN
 from models.bert_lstm import BERT_LSTM
@@ -24,6 +24,8 @@ class Flat_FineTuning(pl.LightningModule):
         
         self.lr = lr
         self.criterion = nn.BCELoss()
+        self.accuracy_metric = BinaryAccuracy()
+        self.mcc_metric = BinaryMatthewsCorrCoef()
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
@@ -34,12 +36,12 @@ class Flat_FineTuning(pl.LightningModule):
         input_ids, target = train_batch
 
         probabilities = self.model(input_ids=input_ids)
-        loss = self.criterion(probabilities.cpu(), target=target.float().cpu())
+        loss = self.criterion(probabilities, target=target.float())
 
-        max_probabilities_idx = probabilities.argmax(1).cpu()
-        max_target_idx = target.argmax(1).cpu()
-        accuracy = accuracy_score(max_target_idx, max_probabilities_idx)
-        mcc = matthews_corrcoef(max_target_idx, max_probabilities_idx)
+        max_probabilities_idx = probabilities.argmax(1)
+        max_target_idx = target.argmax(1)
+        accuracy = self.accuracy_metric(max_probabilities_idx, max_target_idx)
+        mcc = self.mcc_metric(max_probabilities_idx, max_target_idx)
 
         self.log_dict({'train_loss': loss, 'train_accuracy': accuracy, 'train_mcc': mcc}, prog_bar=True, on_epoch=True)
 
@@ -49,12 +51,12 @@ class Flat_FineTuning(pl.LightningModule):
         input_ids, target = valid_batch
 
         probabilities = self.model(input_ids=input_ids)
-        loss = self.criterion(probabilities.cpu(), target=target.float().cpu())
+        loss = self.criterion(probabilities, target=target.float())
 
-        max_probabilities_idx = probabilities.argmax(1).cpu()
-        max_target_idx = target.argmax(1).cpu()
-        accuracy = accuracy_score(max_target_idx, max_probabilities_idx)
-        mcc = matthews_corrcoef(max_target_idx, max_probabilities_idx)
+        max_probabilities_idx = probabilities.argmax(1)
+        max_target_idx = target.argmax(1)
+        accuracy = self.accuracy_metric(max_probabilities_idx, max_target_idx)
+        mcc = self.mcc_metric(max_probabilities_idx, max_target_idx)
 
         self.log_dict({'val_loss': loss, 'val_accuracy': accuracy, 'val_mcc': mcc}, prog_bar=True, on_epoch=True)
 
@@ -64,12 +66,12 @@ class Flat_FineTuning(pl.LightningModule):
         input_ids, target = test_batch
 
         probabilities = self.model(input_ids=input_ids)
-        loss = self.criterion(probabilities.cpu(), target=target.float().cpu())
+        loss = self.criterion(probabilities, target=target.float())
 
-        max_probabilities_idx = probabilities.argmax(1).cpu()
-        max_target_idx = target.argmax(1).cpu()
-        accuracy = accuracy_score(max_target_idx, max_probabilities_idx)
-        mcc = matthews_corrcoef(max_target_idx, max_probabilities_idx)
+        max_probabilities_idx = probabilities.argmax(1)
+        max_target_idx = target.argmax(1)
+        accuracy = self.accuracy_metric(max_probabilities_idx, max_target_idx)
+        mcc = self.mcc_metric(max_probabilities_idx, max_target_idx)
 
         self.log_dict({'test_loss': loss, 'test_accuracy': accuracy, 'test_mcc': mcc}, prog_bar=True, on_epoch=True)
 
