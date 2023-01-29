0f269646717075bf8708bd138b6b1393b14826a6
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-01-29 14:19:06 +0700

    Update tree_generator.py

--- utils/tree_generator.py
+++ utils/tree_generator.py
@@ -3,26 +3,17 @@ import pandas as pd
 class Tree_Generator:
     def __init__(self, dataset):
         hierarchy = []
+        
+        for root in dataset['root'].drop_duplicates().values.tolist():
+            hierarchy.append(root)
 
-        dataset = dataset[['root', 'node', 'leaf']]
-        dataset = dataset.drop_duplicates()
+        for node in dataset['node'].drop_duplicates().values.tolist():
+            hierarchy.append(node)
 
-        target_root = dataset['root'].drop_duplicates().values.tolist()
-        target_node = dataset['node'].drop_duplicates().values.tolist()
-
-        for root in target_root:
-            for node in target_node:
-                for data in dataset.values.tolist():
-                    hierarchy.append(data[0])
-                    if data[0] == root:
-                        if data[1] not in hierarchy:
-                            hierarchy.append(data[1])
-                    if data[1] == node:
-                        if data[2] not in hierarchy:
-                            hierarchy.append(data[2])
+        for leaf in dataset['leaf'].drop_duplicates().values.tolist():
+            hierarchy.append(leaf)
 
         tree = pd.DataFrame([*zip(hierarchy)])
-        tree = tree.drop_duplicates()
         tree = tree.sort_values(tree.columns[0])
 
         tree.to_csv('datasets/labels_hierarchy.tree', header=None, index=False, encoding='utf-8')
