bef99a495f7f5680941a59c8a8fac63e173fa1a3
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-03-23 10:40:08 +0700

    add f1 weighted

--- models/flat_bert_cnn.py
+++ models/flat_bert_cnn.py
@@ -60,8 +60,9 @@ class Flat_Trainer(object):
         accuracy = self.accuracy_metric(preds, target)
         f1_micro = self.f1_micro_metric(preds, target)
         f1_macro = self.f1_macro_metric(preds, target)
+        f1_weighted = self.f1_weighted_metric(preds, target)
 
-        return accuracy, f1_micro, f1_macro
+        return accuracy, f1_micro, f1_macro, f1_weighted
 
     def initialize_model(self, num_classes, train_size):
         self.model = BERT_CNN(num_classes=num_classes, bert_model=self.bert_model, dropout=self.dropout)
@@ -73,6 +74,7 @@ class Flat_Trainer(object):
         self.accuracy_metric = MulticlassAccuracy(num_classes=num_classes).to(self.device)
         self.f1_micro_metric = MulticlassF1Score(num_classes=num_classes, average='micro').to(self.device)
         self.f1_macro_metric = MulticlassF1Score(num_classes=num_classes, average='macro').to(self.device)
+        self.f1_weighted_metric = MulticlassF1Score(num_classes=num_classes, average='weighted').to(self.device)
 
     def training_step(self):
         self.model.train(True)
@@ -81,6 +83,7 @@ class Flat_Trainer(object):
         train_step_accuracy = []
         train_step_f1_micro = []
         train_step_f1_macro = []
+        train_step_f1_weighted = []
 
         training_progress = tqdm(self.train_set)
 
@@ -94,16 +97,18 @@ class Flat_Trainer(object):
             loss = self.criterion(preds, target)
             preds = self.softmax(preds)
 
-            accuracy, f1_micro, f1_macro = self.scoring_result(preds=preds, target=target)
+            accuracy, f1_micro, f1_macro, f1_weighted = self.scoring_result(preds=preds, target=target)
 
             train_step_loss.append(loss.item())
             train_step_accuracy.append(accuracy.item())
             train_step_f1_micro.append(f1_micro.item())
             train_step_f1_macro.append(f1_macro.item())
+            train_step_f1_weighted.append(f1_weighted.item())
             
             training_progress.set_description("Train Step Loss : " + str(round(loss.item(), 2)) + 
                                         " | Train Step Accuracy : " + str(round(accuracy.item(), 2)) + 
                                         " | Train Step F1 Micro : " + str(round(f1_micro.item(), 2)) +
+                                        " | Train Step F1 Weighted : " + str(round(f1_weighted.item(), 2)) +
                                         " | Train Step F1 Macro : " + str(round(f1_macro.item(), 2)))
 
             loss.backward()
@@ -115,8 +120,9 @@ class Flat_Trainer(object):
         print("On Epoch Train Accuracy: ", round(mean(train_step_accuracy), 2))
         print("On Epoch Train F1 Micro: ", round(mean(train_step_f1_micro), 2))
         print("On Epoch Train F1 Macro: ", round(mean(train_step_f1_macro), 2))
+        print("On Epoch Train F1 Weighted: ", round(mean(train_step_f1_weighted), 2))
 
-        return mean(train_step_loss), mean(train_step_accuracy), mean(train_step_f1_micro), mean(train_step_f1_macro)
+        return mean(train_step_loss), mean(train_step_accuracy), mean(train_step_f1_micro), mean(train_step_f1_macro), mean(train_step_f1_weighted)
 
     def validation_step(self):
         self.model.eval()
@@ -125,6 +131,7 @@ class Flat_Trainer(object):
         val_step_accuracy = []
         val_step_f1_micro = []
         val_step_f1_macro = []
+        val_step_f1_weighted = []
 
         with torch.no_grad():
             validation_progress = tqdm(self.valid_set)
@@ -139,16 +146,18 @@ class Flat_Trainer(object):
                 loss = self.criterion(preds, target)
                 preds = self.softmax(preds)
 
-                accuracy, f1_micro, f1_macro = self.scoring_result(preds=preds, target=target)
+                accuracy, f1_micro, f1_macro, f1_weighted = self.scoring_result(preds=preds, target=target)
 
                 val_step_loss.append(loss.item())
                 val_step_accuracy.append(accuracy.item())
                 val_step_f1_micro.append(f1_micro.item())
                 val_step_f1_macro.append(f1_macro.item())
+                val_step_f1_weighted.append(f1_weighted.item())
                 
                 validation_progress.set_description("Validation Step Loss : " + str(round(loss.item(), 2)) + 
                                             " | Validation Step Accuracy : " + str(round(accuracy.item(), 2)) + 
                                             " | Validation Step F1 Micro : " + str(round(f1_micro.item(), 2)) +
+                                            " | Validation Step F1 Weighted : " + str(round(f1_weighted.item(), 2)) +
                                             " | Validation Step F1 Macro : " + str(round(f1_macro.item(), 2)))
             
                 self.model.zero_grad()
@@ -157,8 +166,9 @@ class Flat_Trainer(object):
         print("On Epoch Validation Accuracy: ", round(mean(val_step_accuracy), 2))
         print("On Epoch Validation F1 Micro: ", round(mean(val_step_f1_micro), 2))
         print("On Epoch Validation F1 Macro: ", round(mean(val_step_f1_macro), 2))
+        print("On Epoch Validation F1 Weighted: ", round(mean(val_step_f1_weighted), 2))
 
-        return mean(val_step_loss), mean(val_step_accuracy), mean(val_step_f1_micro), mean(val_step_f1_macro)
+        return mean(val_step_loss), mean(val_step_accuracy), mean(val_step_f1_micro), mean(val_step_f1_macro), mean(val_step_f1_weighted)
     
     def test_step(self):
         self.model.eval()
@@ -167,6 +177,7 @@ class Flat_Trainer(object):
         test_step_accuracy = []
         test_step_f1_micro = []
         test_step_f1_macro = []
+        test_step_f1_weighted = []
 
         with torch.no_grad():
             test_progress = tqdm(self.test_set)
@@ -181,24 +192,27 @@ class Flat_Trainer(object):
                 loss = self.criterion(preds, target)
                 preds = self.softmax(preds)
 
-                accuracy, f1_micro, f1_macro = self.scoring_result(preds=preds, target=target)
+                accuracy, f1_micro, f1_macro, f1_weighted = self.scoring_result(preds=preds, target=target)
 
                 test_step_loss.append(loss.item())
                 test_step_accuracy.append(accuracy.item())
                 test_step_f1_micro.append(f1_micro.item())
                 test_step_f1_macro.append(f1_macro.item())
+                test_step_f1_weighted.append(f1_weighted.item())
                 
                 test_progress.set_description("Test Step Loss : " + str(round(loss.item(), 2)) + 
                                             " | Test Step Accuracy : " + str(round(accuracy.item(), 2)) + 
                                             " | Test Step F1 Micro : " + str(round(f1_micro.item(), 2)) +
+                                            " | Test Step F1 Weighted : " + str(round(f1_weighted.item(), 2)) +
                                             " | Test Step F1 Macro : " + str(round(f1_macro.item(), 2)))
 
         print("On Epoch Test Loss: ", round(mean(test_step_loss), 2))
         print("On Epoch Test Accuracy: ", round(mean(test_step_accuracy), 2))
         print("On Epoch Test F1 Micro: ", round(mean(test_step_f1_micro), 2))
         print("On Epoch Test F1 Macro: ", round(mean(test_step_f1_macro), 2))
+        print("On Epoch Test F1 Weighted: ", round(mean(test_step_f1_weighted), 2))
 
-        return mean(test_step_loss), mean(test_step_accuracy), mean(test_step_f1_micro), mean(test_step_f1_macro)
+        return mean(test_step_loss), mean(test_step_accuracy), mean(test_step_f1_micro), mean(test_step_f1_macro), mean(test_step_f1_weighted)
     
     def fit(self, datamodule):
         level_on_nodes_indexed, _, _ = self.tree.generate_hierarchy()
@@ -207,12 +221,14 @@ class Flat_Trainer(object):
         train_loss_epoch = []
         train_f1_micro_epoch = []
         train_f1_macro_epoch = []
+        train_f1_weighted_epoch = []
         train_epoch = []
 
         val_accuracy_epoch = []
         val_loss_epoch = []
         val_f1_micro_epoch = []
         val_f1_macro_epoch = []
+        val_f1_weighted_epoch = []
         val_epoch = []
 
         self.train_set, self.valid_set = datamodule.flat_dataloader(stage='fit')
@@ -226,23 +242,25 @@ class Flat_Trainer(object):
             print("Epoch ", epoch)
             print("=" * 50)
 
-            train_loss, train_accuracy, train_f1_micro, train_f1_macro = self.training_step()
+            train_loss, train_accuracy, train_f1_micro, train_f1_macro, train_f1_weighted = self.training_step()
 
             train_loss_epoch.append(train_loss)
             train_accuracy_epoch.append(train_accuracy)
             train_f1_micro_epoch.append(train_f1_micro)
             train_f1_macro_epoch.append(train_f1_macro)
+            train_f1_weighted_epoch.append(train_f1_weighted)
             train_epoch.append(epoch)
 
             print("Validation Stage...")
             print("=" * 50)
 
-            val_loss, val_accuracy, val_f1_micro, val_f1_macro = self.validation_step()
+            val_loss, val_accuracy, val_f1_micro, val_f1_macro, val_f1_weighted = self.validation_step()
 
             val_loss_epoch.append(val_loss)
             val_accuracy_epoch.append(val_accuracy)
             val_f1_micro_epoch.append(val_f1_micro)
             val_f1_macro_epoch.append(val_f1_macro)
+            val_f1_weighted_epoch.append(val_f1_weighted)
             val_epoch.append(epoch)
 
             if round(val_loss, 2) < round(best_loss, 2):
@@ -263,8 +281,8 @@ class Flat_Trainer(object):
         if not os.path.exists('logs/flat_result'):
             os.makedirs('logs/flat_result')
             
-        train_result = pd.DataFrame({'epoch': train_epoch, 'accuracy': train_accuracy_epoch, 'loss': train_loss_epoch, 'f1_micro': train_f1_micro_epoch, 'f1_macro': train_f1_macro_epoch})
-        valid_result = pd.DataFrame({'epoch': val_epoch, 'accuracy': val_accuracy_epoch, 'loss': val_loss_epoch, 'f1_micro': val_f1_micro_epoch, 'f1_macro': val_f1_macro_epoch})
+        train_result = pd.DataFrame({'epoch': train_epoch, 'accuracy': train_accuracy_epoch, 'loss': train_loss_epoch, 'f1_micro': train_f1_micro_epoch, 'f1_macro': train_f1_macro_epoch, 'f1_weighted': train_f1_weighted_epoch})
+        valid_result = pd.DataFrame({'epoch': val_epoch, 'accuracy': val_accuracy_epoch, 'loss': val_loss_epoch, 'f1_micro': val_f1_micro_epoch, 'f1_macro': val_f1_macro_epoch, 'f1_weighted': val_f1_weighted_epoch})
         
         train_result.to_csv('logs/flat_result/train_result.csv', index=False, encoding='utf-8')
         valid_result.to_csv('logs/flat_result/valid_result.csv', index=False, encoding='utf-8')
@@ -274,6 +292,7 @@ class Flat_Trainer(object):
         test_loss_epoch = []
         test_f1_micro_epoch = []
         test_f1_macro_epoch = []
+        test_f1_weighted_epoch = []
 
         checkpoint = torch.load('checkpoints/flat_result/temp.pt')
         self.model.load_state_dict(checkpoint['model_state'])
@@ -284,17 +303,18 @@ class Flat_Trainer(object):
         print("Loading Checkpoint on Epoch", checkpoint['epoch'])
         print("=" * 50)
 
-        test_loss, test_accuracy, test_f1_micro, test_f1_macro = self.test_step()
+        test_loss, test_accuracy, test_f1_micro, test_f1_macro, test_f1_weighted = self.test_step()
         
         test_loss_epoch.append(test_loss)
         test_accuracy_epoch.append(test_accuracy)
         test_f1_micro_epoch.append(test_f1_micro)
         test_f1_macro_epoch.append(test_f1_macro)
+        test_f1_weighted_epoch.append(test_f1_weighted)
 
         if not os.path.exists('logs/flat_result'):
             os.makedirs('logs/flat_result')
                         
-        test_result = pd.DataFrame({'accuracy': test_accuracy_epoch, 'loss': test_loss_epoch, 'f1_micro': test_f1_micro_epoch, 'f1_macro': test_f1_macro_epoch})
+        test_result = pd.DataFrame({'accuracy': test_accuracy_epoch, 'loss': test_loss_epoch, 'f1_micro': test_f1_micro_epoch, 'f1_macro': test_f1_macro_epoch, 'f1_weighted': test_f1_weighted_epoch})
         test_result.to_csv('logs/flat_result/test_result.csv', index=False, encoding='utf-8')
 
     def create_graph(self):
@@ -302,7 +322,7 @@ class Flat_Trainer(object):
         train_log = pd.read_csv('logs/flat_result/train_result.csv')
         valid_log = pd.read_csv('logs/flat_result/valid_result.csv')
 
-        for metric in ['accuracy', 'loss', 'f1_micro', 'f1_macro']:
+        for metric in ['accuracy', 'loss', 'f1_micro', 'f1_macro', 'f1_weighted']:
             plt.xlabel('epoch')
             plt.ylabel(metric.replace("_", " ").title())
             plt.plot(train_log['epoch'], train_log[metric], marker='o', label='Train')
