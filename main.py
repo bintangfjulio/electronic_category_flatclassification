1ade13dbcdc918bde6c1e3daf0cec5d21e233fc6
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-03-23 11:12:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--bert_model", type=str, default='indolem/indobert-base-uncased')
     parser.add_argument("--seed", type=int, default=42)
-    parser.add_argument("--max_epochs", type=int, default=4)
+    parser.add_argument("--max_epochs", type=int, default=20)
     parser.add_argument("--lr", type=float, default=2e-5)
     parser.add_argument("--dropout", type=float, default=0.1)
     parser.add_argument("--patience", type=int, default=2)
