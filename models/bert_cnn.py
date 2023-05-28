a0395d4d207791d640c915a2b2033cceca628c2a
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2023-05-28 11:33:57 +0700

    Update bert_cnn.py

--- models/bert_cnn.py
+++ models/bert_cnn.py
@@ -5,16 +5,19 @@ import torch.nn.functional as F
 from transformers import BertModel
 
 class BERT_CNN(nn.Module):
-    def __init__(self, num_classes, bert_model, dropout, input_size=768, window_sizes=[1, 2, 3, 4, 5], in_channels=4, out_channels=32):
+    def __init__(self, num_classes, bert_model, dropout, level=None, input_size=768, window_sizes=[1, 2, 3, 4, 5], in_channels=4, out_channels=32):
         super(BERT_CNN, self).__init__()
         self.pretrained_bert = BertModel.from_pretrained(bert_model, output_hidden_states=True)
         self.convolutional_layers = nn.ModuleList([nn.Conv2d(in_channels, out_channels, (window_size, input_size)) for window_size in window_sizes])
         self.dropout = nn.Dropout(dropout) 
-        self.output_layer = nn.Linear(len(window_sizes) * out_channels, num_classes)
         self.window_length = len(window_sizes)
         self.out_channels_length = out_channels
+        self.level = level
 
-    def forward(self, input_ids, level=False):
+        if self.level is None:
+           self.output_layer = nn.Linear(len(window_sizes) * out_channels, num_classes)
+
+    def forward(self, input_ids):
         bert_output = self.pretrained_bert(input_ids=input_ids)
         bert_hidden_states = bert_output[2]
         bert_hidden_states = torch.stack(bert_hidden_states, dim=1)
@@ -26,7 +29,7 @@ class BERT_CNN(nn.Module):
         flatten = torch.cat(max_pooler, dim=1)
         logits = self.dropout(flatten)
 
-        if level:
+        if self.level is not None:
             return logits
 
         preds = self.output_layer(logits)
